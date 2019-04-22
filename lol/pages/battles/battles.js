712110922f50f982e442a447639880d720cb294a
unknown <2434874038@qq.com>
2019-04-22 23:21:34 +0800

    first

---
+++ lol/pages/battles/battles.js
@@ -0,0 +1,26 @@
+Page({
+    data: {
+        battles: [
+            {
+                name: "英雄",
+                militarySuccesses:'胜率'
+            },
+            {
+                name: "阿卡丽",
+                militarySuccesses:'60%'
+            },
+            {
+                name: "亚索",
+                militarySuccesses:'66%'
+            },
+            {
+                name: "盖伦",
+                militarySuccesses:'58%'
+            },
+            {
+                name: "火男",
+                militarySuccesses:'77%'
+            }
+        ]
+    }
+})
